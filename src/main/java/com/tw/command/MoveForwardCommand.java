e945facebb19c794aaafc94a8e19271e7aa4d3e1
qyfei <qyfei@thoughtworks.com>
2020-01-15 23:46:56 +0800

    use function interface to avoid if else logic

--- src/main/java/com/tw/command/MoveForwardCommand.java
+++ src/main/java/com/tw/command/MoveForwardCommand.java
@@ -3,17 +3,36 @@ package com.tw.command;
 import com.tw.model.Direction;
 import com.tw.model.RoverStatus;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public class MoveForwardCommand implements Command {
+
+  private static Function<RoverStatus, RoverStatus> addX =
+          roverStatus -> new RoverStatus(roverStatus.getX() + 1d, roverStatus.getY(), roverStatus.getDirection());
+
+  private static Function<RoverStatus, RoverStatus> minusX =
+          roverStatus -> new RoverStatus(roverStatus.getX() - 1d, roverStatus.getY(), roverStatus.getDirection());
+
+  private static Function<RoverStatus, RoverStatus> addY =
+          roverStatus -> new RoverStatus(roverStatus.getX(), roverStatus.getY() + 1d, roverStatus.getDirection());
+
+  private static Function<RoverStatus, RoverStatus> minusY =
+          roverStatus -> new RoverStatus(roverStatus.getX(), roverStatus.getY() - 1d, roverStatus.getDirection());
+
+  private static Map<Direction, Function<RoverStatus, RoverStatus>> functionMap;
+
+  static {
+    functionMap = new HashMap<>();
+    functionMap.put(Direction.NORTH, addY);
+    functionMap.put(Direction.SOUTH, minusY);
+    functionMap.put(Direction.WEST, minusX);
+    functionMap.put(Direction.EAST, addX);
+  }
+
   @Override
   public RoverStatus doAction(RoverStatus roverStatus) {
-    if(roverStatus.getDirection().equals(Direction.NORTH)) {
-      return new RoverStatus(roverStatus.getX(), roverStatus.getY() + 1, roverStatus.getDirection());
-    } else if(roverStatus.getDirection().equals(Direction.SOUTH)) {
-      return new RoverStatus(roverStatus.getX(), roverStatus.getY() - 1, roverStatus.getDirection());
-    } else if(roverStatus.getDirection().equals(Direction.WEST)) {
-      return new RoverStatus(roverStatus.getX() - 1, roverStatus.getY(), roverStatus.getDirection());
-    } else {
-      return new RoverStatus(roverStatus.getX() + 1, roverStatus.getY(), roverStatus.getDirection());
-    }
+    return functionMap.get(roverStatus.getDirection()).apply(roverStatus);
   }
 }
